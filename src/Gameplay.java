8e493f86c37d5390f65b522d2ee9231a7dec6367
asimiea <asimieasobomate@gmail.com>
2021-08-20 00:56:45 -0400

    Added movement of ball

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -5,9 +5,9 @@
 import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.Shape;
+import javafx.scene.input.KeyCode;
 
 public class Gameplay {
     private Pane root;
@@ -21,18 +21,35 @@ public class Gameplay {
     public Gameplay(){
         root = new Pane();
         scene = new Scene(root, configuration.width, configuration.height);
-        scene.setFill(Color.PURPLE);
+        new Background(root);
 
-        rect = new Rectangle(10, 50);
-        //createGameElements();
+        setupKeybinds();
+        createGameElements();
+    }
+
+    private void setupKeybinds() {
+        scene.setOnKeyPressed(e -> {
+            if(e.getCode() == KeyCode.LEFT &&ball.getX() > -30){
+               ball.setX(ball.getX()-10);
+            }
+            if(e.getCode() == KeyCode.RIGHT &&ball.getX() < 460){
+               ball.setX(ball.getX()+10);
+            }
+        });
     }
 
     public void createGameElements(){
         //platform
-        rect = new Rectangle(10, 50);
+        rect = new Rectangle(1, 5);
+        //root.getChildren().add(rect);
 
         //ball
         ball = new ImageView(configuration.ball);
+        ball.setFitWidth(25);
+        ball.setPreserveRatio(true);
+        ball.setSmooth(true);
+        ball.setCache(true);
+        root.getChildren().add(ball);
     }
 
     public static Scene getScene(){
